1116c9d8d738da070cd7e056168d450475c7d6b0
riya-bubna <bubna.riya13@gmail.com>
2018-02-24 21:26:37 +0530

    Added splash screen

--- app/src/main/java/com/example/vaishali/gosafe/SplashActivity.java
+++ app/src/main/java/com/example/vaishali/gosafe/SplashActivity.java
@@ -1,17 +1,31 @@
 package com.example.vaishali.gosafe;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
-        finish();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent LoginActivity = new Intent(SplashActivity.this, MapsActivity.class);
+                startActivity(LoginActivity);
+                finish();
+            }
+        }, 2000);
+
     }
 }
 
